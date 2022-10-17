ee6dfca20e00e9948473abbadc7d593338f35705
aubin-tchoi <aubin.tchoi@gmail.com>
2022-10-17 15:50:03 +0200

    mailed info also includes room number + time comparison fixed

--- sync_changes.js
+++ sync_changes.js
@@ -29,39 +29,69 @@ function report_course_movements() {
         ...Object.keys(local_minus_mva)
     ])].forEach(
         title => {
-            if (local_minus_mva[title].length === 1 && mva_minus_local[title].length === 1) {
+            if (local_minus_mva[title]?.length === 1 && mva_minus_local[title]?.length === 1) {
                 modified = true;
                 let location_change = (
-                    local_minus_mva[title][0].getLocation() !== mva_minus_local[title][0].getLocation()
-                        ? `<br/>&nbsp;&nbsp;- Old location: ${local_minus_mva[title][0].getLocation()}<br/>
-                        &nbsp;&nbsp;- New location: ${mva_minus_local[title][0].getLocation()}`
-                        : ""
+                        (local_minus_mva[title][0].getLocation() !== mva_minus_local[title][0].getLocation()
+                            || local_minus_mva[title][0].getDescription() !== mva_minus_local[title][0].getDescription())
+                            ? `<br/>&nbsp;&nbsp;- Old location: ${local_minus_mva[title][0].getLocation()} ${local_minus_mva[title][0].getDescription()}<br/>
+                        &nbsp;&nbsp;- New location: ${mva_minus_local[title][0].getLocation()} ${mva_minus_local[title][0].getDescription()}`
+                            : ""
                     ),
                     time_change = (
-                    local_minus_mva[title][0].getStartTime() !== mva_minus_local[title][0].getStartTime()
-                        ? `<br/>&nbsp;&nbsp;- Old time: ${local_minus_mva[title][0].getStartTime()} to 
+                        local_minus_mva[title][0].getStartTime().toLocaleString() !== mva_minus_local[title][0].getStartTime().toLocaleString()
+                            ? `<br/>&nbsp;&nbsp;- Old time: ${local_minus_mva[title][0].getStartTime()} to 
                         ${local_minus_mva[title][0].getEndTime()}<br/>
                         &nbsp;&nbsp;- New time: ${mva_minus_local[title][0].getStartTime()} to
                         ${mva_minus_local[title][0].getEndTime()}`
-                        : ""
+                            : ""
                     );
                 htmlOutput.append(
                     `The following course was modified:<br/>
                 &nbsp;${title}${location_change}${time_change}<br/><br/>`
                 );
-            } else if (local_minus_mva[title] === undefined && mva_minus_local[title].length > 0) {
+                Logger.log(`Updating the course info for ${title}.`);
+                local_minus_mva[title][0].setTime(
+                    mva_minus_local[title][0].getStartTime(),
+                    mva_minus_local[title][0].getEndTime()
+                );
+                local_minus_mva[title][0].setDescription(
+                    mva_minus_local[title][0].getDescription()
+                );
+            } else if (
+                local_minus_mva[title] === undefined
+                && mva_minus_local[title]?.length > 0
+                && !title.startsWith("ANNULE - ")
+            ) {
                 modified = true;
                 htmlOutput.append(
                     `The following course was added:<br/>
                 &nbsp;- ${mva_minus_local[title].map(get_course_info).join("<br/>&nbsp;- ")}<br/><br/>`
                 );
-            } else if (local_minus_mva[title].length > 0 && mva_minus_local[title] === undefined) {
+                Logger.log(`Adding course info for ${title}.`);
+                mva_minus_local[title].forEach(
+                    ev => {
+                        local_calendar.createEvent(
+                            ev.getTitle(),
+                            ev.getStartTime(),
+                            ev.getEndTime(), {
+                                description: ev.getDescription(),
+                                location: ev.getLocation(),
+                            }
+                        );
+                    }
+                );
+            } else if (local_minus_mva[title]?.length > 0 && mva_minus_local[title] === undefined) {
                 modified = true;
                 htmlOutput.append(
                     `The following course was removed:<br/>
                 &nbsp;- ${local_minus_mva[title].map(get_course_info).join("<br/>&nbsp;- ")}<br/><br/>`
                 );
-            } else if (local_minus_mva[title].length > 0 && mva_minus_local[title].length > 0) {
+                Logger.log(`Removing course event for ${title}.`);
+                local_minus_mva[title].forEach(ev => {
+                    ev.deleteEvent()
+                });
+            } else if (local_minus_mva[title]?.length > 0 && mva_minus_local[title]?.length > 0) {
                 modified = true;
                 htmlOutput.append(
                     `The courses ${title} were moved around.<br/><br/>`
